fc9dbfa555c809e125c9f5c7229c477e57f13528
Sebastien Deleuze <seb@deleuze.fr>
2016-12-08 22:34:03 +0100

    Add Spring Web Functional based HTTP server

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit
+
 buildscript {
 	val kotlinVersion = "1.0.5-2"
 	extra["kotlinVersion"] = kotlinVersion
@@ -19,14 +21,50 @@ apply {
 
 version = "1.0.0-SNAPSHOT"
 
+
 repositories {
 	mavenCentral()
+	maven { setUrl("https://repo.spring.io/snapshot") }
+}
+
+configure<JavaPluginConvention> {
+	setSourceCompatibility(1.8)
+	setTargetCompatibility(1.8)
+}
+
+configurations.all {
+	it.resolutionStrategy.cacheChangingModulesFor(0, TimeUnit.SECONDS)
 }
 
 val kotlinVersion = extra["kotlinVersion"] as String
+val springVersion = "5.0.0.BUILD-SNAPSHOT"
+val jacksonVersion = "2.8.4"
+val reactorVersion = "3.0.3.RELEASE"
 
 dependencies {
-	compile("org.jetbrains.kotlin:kotlin-stdlib:${kotlinVersion}")
+	compile("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
+
+	compile("org.springframework:spring-aop:$springVersion")
+	compile("org.springframework:spring-beans:$springVersion")
+	compile("org.springframework:spring-context:$springVersion")
+	compile("org.springframework:spring-core:$springVersion")
+	compile("org.springframework:spring-expression:$springVersion")
+	compile("org.springframework:spring-web:$springVersion")
+	compile("org.springframework:spring-web-reactive:$springVersion")
+
+	compile("io.projectreactor:reactor-core:$reactorVersion")
+
+	compile("com.fasterxml.jackson.core:jackson-annotations:$jacksonVersion")
+	compile("com.fasterxml.jackson.core:jackson-core:$jacksonVersion")
+	compile("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+	compile("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
+	compile("io.projectreactor.ipc:reactor-netty:0.6.0.BUILD-SNAPSHOT")
+
+	compile("commons-logging:commons-logging:1.2")
+	compile("org.slf4j:slf4j-api:1.7.21")
+	compile("ch.qos.logback:logback-classic:1.1.7")
+
 }
 
 
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,3 +1,25 @@
+import org.springframework.web.reactive.function.RouterFunction
+import org.springframework.http.codec.BodyInserters
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter
+import org.springframework.web.reactive.function.HandlerFunction
+import org.springframework.web.reactive.function.RequestPredicates.GET
+import org.springframework.web.reactive.function.RouterFunctions
+import org.springframework.web.reactive.function.RouterFunctions.route
+import org.springframework.web.reactive.function.ServerResponse.ok
+import reactor.ipc.netty.http.server.HttpServer
+
+
 fun main(args: Array<String>) {
-    println("Hello Mix-It!")
+    val httpHandler = RouterFunctions.toHttpHandler(routes())
+    HttpServer.create("0.0.0.0")
+            .newHandler(ReactorHttpHandlerAdapter(httpHandler))
+            .doOnNext({ foo -> println("Server listening on " + foo.address()) })
+            .block()
+            .onClose()
+            .block()
+}
+
+private fun routes(): RouterFunction<*> {
+    return route(GET("/"),
+            HandlerFunction { request -> ok().body(BodyInserters.fromObject("Hello Mix-IT!")) })
 }
