6a8d3f41baa6abbecd821ab34a4c2035342ceaad
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-02-02 01:48:01 +0100

    Polishing

--- src/test/kotlin/mixit/integration/AbstractIntegrationTests.kt
+++ src/test/kotlin/mixit/integration/AbstractIntegrationTests.kt
@@ -4,11 +4,12 @@ import org.junit.Before
 import org.junit.runner.RunWith
 import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.web.reactive.function.client.WebClient
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 abstract class AbstractIntegrationTests {
 
     @LocalServerPort
