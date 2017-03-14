8dc991a842d10e858b014dc91ae2897d05f9f7ca
Sebastien Deleuze <sdeleuze@pivotal.io>
2017-03-14 09:12:56 +0100

    Update LocalServerPort against latest Boot snapshots

--- src/test/kotlin/mixit/integration/AbstractIntegrationTests.kt
+++ src/test/kotlin/mixit/integration/AbstractIntegrationTests.kt
@@ -2,9 +2,9 @@ package mixit.integration
 
 import org.junit.Before
 import org.junit.runner.RunWith
-import org.springframework.boot.context.embedded.LocalServerPort
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
+import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.web.reactive.function.client.WebClient
 
