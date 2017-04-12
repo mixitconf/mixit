37e55b3c56d2cec5dc4bfbc54187fcf28e62b0fd
Guillaume EHRET <guillaume@dev-mind.fr>
2017-04-12 19:26:40 +0200

    Updates the controller to return a map with talk by day and by room

--- src/main/kotlin/mixit/web/handler/TalkHandler.kt
+++ src/main/kotlin/mixit/web/handler/TalkHandler.kt
@@ -9,8 +9,11 @@ import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.*
 import org.springframework.web.reactive.function.server.ServerResponse.*
 import org.springframework.web.util.UriUtils
+import reactor.core.publisher.toMono
 import java.nio.charset.StandardCharsets
 import java.time.LocalDateTime
+import java.time.LocalDate
+import kotlin.streams.toList
 
 
 @Component
@@ -23,27 +26,30 @@ class TalkHandler(val repository: TalkRepository,
             Pair("talks", repository
                     .findByEvent(year.toString(), topic)
                     .collectList()
-                    .flatMap { talks -> userRepository
-                            .findMany(talks.flatMap(Talk::speakerIds))
-                            .collectMap(User::login)
-                            .map { speakers -> talks.map { it.toDto(req.language(), it.speakerIds.mapNotNull { speakers[it] }, markdownConverter) } }
+                    .flatMap { talks ->
+                        userRepository
+                                .findMany(talks.flatMap(Talk::speakerIds))
+                                .collectMap(User::login)
+                                .map { speakers -> talks.map { it.toDto(req.language(), it.speakerIds.mapNotNull { speakers[it] }, markdownConverter) } }
                     }),
             Pair("year", year),
-            Pair("title", when(topic) { null -> "talks.title.html|$year" else -> "talks.title.html.$topic|$year" }),
+            Pair("title", when (topic) { null -> "talks.title.html|$year"
+                else -> "talks.title.html.$topic|$year"
+            }),
             Pair("baseUri", UriUtils.encode(properties.baseUri, StandardCharsets.UTF_8)),
             Pair("topic", topic)
     ))
 
 
-
     fun findOneView(year: Int, req: ServerRequest) = repository.findByEventAndSlug(year.toString(), req.pathVariable("slug")).flatMap { talk ->
         userRepository.findMany(talk.speakerIds).collectList().flatMap { speakers ->
-        ok().render("talk", mapOf(
-                Pair("talk", talk.toDto(req.language(), speakers!!, markdownConverter)),
-                Pair("speakers", speakers.map { it.toDto(req.language(), markdownConverter) }),
-                Pair("title", "talk.html.title|${talk.title}"),
-                Pair("baseUri", UriUtils.encode(properties.baseUri, StandardCharsets.UTF_8))))
-    }}
+            ok().render("talk", mapOf(
+                    Pair("talk", talk.toDto(req.language(), speakers!!, markdownConverter)),
+                    Pair("speakers", speakers.map { it.toDto(req.language(), markdownConverter) }),
+                    Pair("title", "talk.html.title|${talk.title}"),
+                    Pair("baseUri", UriUtils.encode(properties.baseUri, StandardCharsets.UTF_8))))
+        }
+    }
 
     fun findOne(req: ServerRequest) = ok().json().body(repository.findOne(req.pathVariable("login")))
 
@@ -58,8 +64,56 @@ class TalkHandler(val repository: TalkRepository,
         permanentRedirect("${properties.baseUri}/${it.event}/${it.slug}")
     }
 
-    fun planning(req: ServerRequest) = ok().render("planning")
+    fun planning(req: ServerRequest) = ok().render("planning", mapOf(
+            Pair("talks", repository
+                    .findByEvent("2017", null)
+                    .collectList()
+                    .flatMap { talks -> createTalkMapByDateAndRoom(talks, req.language()).toMono()}),
+
+            Pair("baseUri", UriUtils.encode(properties.baseUri, StandardCharsets.UTF_8))
+    ))
+
+    private fun createTalkMapByDateAndRoom(talks: List<Talk>, lang: Language): Map<String, Map<String, List<TalkDto>>> = talks
+            .map { it.start!!.toLocalDate() }
+            .distinct()
+            .map { createTalksByDate(it, talks) }
+            .associateBy({ "J${it.date.dayOfMonth}" }, { it.talks })
+            .entries
+            .associateBy({ it.key }, { createTalksByRoom(it.value, lang) })
+
+    private fun createTalksByRoom(talksByDate: List<Talk>, lang: Language): Map<String, List<TalkDto>> =
+            listOf(Room.AMPHI1, Room.AMPHI2, Room.ROOM1, Room.ROOM2, Room.ROOM3, Room.ROOM4, Room.ROOM5, Room.ROOM6)
+                    .stream()
+                    .map { createTalksByRoom(it, talksByDate) }
+                    .toList()
+                    .associateBy({ it.room.name }, { it.talks.map {
+                        talk -> userRepository.findMany(talk.speakerIds)
+                            .collectMap(User::login)
+                            .map { speakers -> talk.toDto(lang, talk.speakerIds.mapNotNull { speakers[it] }, markdownConverter) }
+                            .block()
+                    } })
+
+    private fun createTalksByDate(date: LocalDate, talks: List<Talk>) = TalkByDateDto(
+            date,
+            talks.filter { it.start!!.toLocalDate().equals(date) }
+    )
+
+    private fun createTalksByRoom(room: Room, talks: List<Talk>) = TalkByRoomDto(
+            room,
+            talks.filter { it.room == room }.sortedBy { it.start }
+    )
+
 }
+
+class TalkByDateDto(
+        val date:LocalDate,
+        val talks: List<Talk>
+)
+
+class TalkByRoomDto(
+        val room:Room,
+        val talks: List<Talk>
+)
 
 class TalkDto(
         val id: String?,
