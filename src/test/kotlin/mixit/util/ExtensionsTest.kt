ea189fa2e63c4752d0bf429c7720b35af5e00b99
Guillaume EHRET <guillaume@dev-mind.fr>
2017-11-15 09:20:02 +0100

    Add utility to compute the md5 of a String

--- src/test/kotlin/mixit/util/ExtensionsTest.kt
+++ src/test/kotlin/mixit/util/ExtensionsTest.kt
@@ -1,6 +1,7 @@
 package mixit.util
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
 /**
@@ -16,4 +17,12 @@ class ExtensionsTest {
         assertEquals("mixit-2017-ticketing-pre-registration", "MiXiT 2017 ticketing: pre-registration".toSlug())
     }
 
+    @Test
+    fun md5() {
+        assertEquals("aa4d47d1016e45c23b6af05ec11c0a9c", "gui.ehret@gmail.com".md5Hex())
+        val nullable:String? = null
+        assertNull(nullable?.md5Hex())
+        val empty = ""
+        assertNull(empty.md5Hex())
+    }
 }
