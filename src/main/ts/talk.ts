4691a70bba4d5f05f2f842777d69c7bbeb7c28d0
Guillaume EHRET <guillaume@dev-mind.fr>
2017-12-12 10:33:34 +0100

    Fix compilation error in typescript files

--- src/main/ts/talk.ts
+++ src/main/ts/talk.ts
@@ -11,7 +11,7 @@ class TalkCtrl{
 
     fetch(`/api/favorites/${email.value}/talks/${talkField.value}/toggle`, {method: 'post'})
       .then(response => response.json())
-      .then(json => {
+      .then((json:any) => {
         const imgPath = json.selected ? 'mxt-favorite.svg' : 'mxt-favorite-non.svg';
         img.src = `/images/svg/favorites/${imgPath}`;
       });
--- src/main/ts/talks.ts
+++ src/main/ts/talks.ts
@@ -1,7 +1,7 @@
 class TalksCtrl{
   constructor() {
     const favoriteButtons = document.getElementsByClassName('mxt-img--favorite');
-    Array.from(favoriteButtons).forEach((favoriteButton: HTMLElement) => favoriteButton.onclick= this.favoriteToggle);
+      Array.from(favoriteButtons).forEach((favoriteButton: HTMLElement) => favoriteButton.onclick= this.favoriteToggle);
   }
 
   favoriteToggle(event) {
@@ -10,7 +10,7 @@ class TalksCtrl{
 
     fetch(`/api/favorites/${email.value}/talks/${img.id.substr(9,img.id.length)}/toggle`, {method: 'post'})
       .then(response => response.json())
-      .then(json => {
+      .then((json: any) => {
         const imgPath = json.selected ? 'mxt-favorite.svg' : 'mxt-favorite-non.svg';
         img.src = `/images/svg/favorites/${imgPath}`;
       });
