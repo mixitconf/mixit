1f56988e1e712302e3762299ea532eeda0988955
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-04-04 21:53:07 +0200

    Updates planning

--- src/main/kotlin/mixit/MixitApplication.kt
+++ src/main/kotlin/mixit/MixitApplication.kt
@@ -1,21 +1,8 @@
 package mixit
 
-import com.google.api.client.auth.oauth2.Credential
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.json.jackson2.JacksonFactory
-import com.google.api.client.util.store.MemoryDataStoreFactory
-import com.google.api.services.gmail.Gmail
-import com.google.api.services.gmail.GmailScopes
-import com.samskivert.mustache.Mustache
-import com.samskivert.mustache.Mustache.TemplateLoader
-import mixit.web.StringEscapers
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Profile
-import org.springframework.core.io.ClassPathResource
 
 
 @SpringBootApplication
