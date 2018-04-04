26fec8647744a471f717b18cc7758dcc417fbd79
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-04-04 23:08:37 +0200

    Update data for bad id

--- src/main/kotlin/mixit/repository/EventRepository.kt
+++ src/main/kotlin/mixit/repository/EventRepository.kt
@@ -3,6 +3,9 @@ package mixit.repository
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import mixit.model.Event
+import mixit.model.EventSponsoring
+import mixit.model.SponsorshipLevel
+import mixit.util.seeOther
 import org.slf4j.LoggerFactory
 import org.springframework.core.io.ClassPathResource
 import org.springframework.data.domain.Sort
@@ -11,6 +14,7 @@ import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.isEqualTo
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 
 
 @Repository
@@ -26,6 +30,19 @@ class EventRepository(private val template: ReactiveMongoTemplate,
             events.forEach { save(it).block() }
             logger.info("Events data initialization complete")
         }
+
+        findAll().collectList().block()?.stream()?.forEach {  event ->
+            if(event.sponsors.any { it.sponsorId.equals("Zenika Lyon") }) {
+                val zenika = event.sponsors.filter { it.sponsorId.equals("Zenika Lyon") }.last()
+                if (zenika != null) {
+                    val sponsors = event.sponsors.filter { !it.sponsorId.equals("Zenika Lyon") }.toMutableList()
+                    sponsors.add(EventSponsoring(zenika.level, "ZenikaLyon", zenika.subscriptionDate))
+
+                    save(Event(event.id, event.start, event.end, event.current, sponsors)).block()
+                }
+            }
+        }
+
     }
 
     fun count() = template.count<Event>()
