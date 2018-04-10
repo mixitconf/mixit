a003084706906daa95b3db3008877c07b6ff55b7
Guillaume EHRET Dev Mind <guillaume@dev-mind.fr>
2018-04-10 13:28:55 +0200

    Fix error with favorites on Firefox

--- src/main/ts/talk.ts
+++ src/main/ts/talk.ts
@@ -1,22 +1,31 @@
-class TalkCtrl{
-  constructor() {
-    const favoriteButton = document.getElementById('favorite');
-    favoriteButton.onclick = this.favoriteToggle;
-  }
+class TalkCtrl {
+    constructor() {
+        const favoriteButton = document.getElementById('favorite');
+        favoriteButton.onclick = this.favoriteToggle;
+    }
 
-  favoriteToggle(event) {
-    const talkField = <HTMLInputElement> document.getElementById('talkId');
-    const email = <HTMLInputElement> document.getElementById('email');
-    const img = event.srcElement;
-    event.stopPropagation();
+    favoriteToggle(event) {
+        // Depending on the browser the target is not the same. In Firefox this is the button and in Chrome the img
+        const elt = event.target;
+        const targetIsButton = event.target.outerHTML.indexOf('button') >= 0;
 
-    fetch(`/api/favorites/${email.value}/talks/${talkField.value}/toggle`, {method: 'post'})
-      .then(response => response.json())
-      .then((json:any) => {
-        const imgPath = json.selected ? 'mxt-favorite.svg' : 'mxt-favorite-non.svg';
-        img.src = `/images/svg/favorites/${imgPath}`;
-      });
-  }
+        const talkField = <HTMLInputElement> document.getElementById('talkId');
+        const email = <HTMLInputElement> document.getElementById('email');
+
+        fetch(`/api/favorites/${email.value}/talks/${talkField.value}/toggle`, {method: 'post'})
+            .then(response => response.json())
+            .then((json: any) => {
+                const imgPath = json.selected ? 'mxt-favorite.svg' : 'mxt-favorite-non.svg';
+                if (targetIsButton) {
+                    elt.innerHTML = `<img src="/images/svg/favorites/${imgPath}" class="mxt-icon--cat__talks" id="favorite-{{id}}"/>`;
+                }
+                else {
+                    elt.src = `/images/svg/favorites/${imgPath}`;
+                }
+            });
+
+        event.stopPropagation();
+    }
 }
 
 window.addEventListener("load", () => new TalkCtrl());
--- src/main/ts/talks.ts
+++ src/main/ts/talks.ts
@@ -1,17 +1,27 @@
 class TalksCtrl{
 
-  favoriteToggle(event) {
-    const img = event.srcElement;
-    const email = <HTMLInputElement> document.getElementById('email');
+    favoriteToggle(event){
+        // Depending on the browser the target is not the same. In Firefox this is the button and in Chrome the img
+        const elt = event.target;
+        const targetIsButton = event.target.outerHTML.indexOf('button') >= 0;
 
-    fetch(`/api/favorites/${email.value}/talks/${img.id.substr(9,img.id.length)}/toggle`, {method: 'post'})
-      .then(response => response.json())
-      .then((json: any) => {
-        const imgPath = json.selected ? 'mxt-favorite.svg' : 'mxt-favorite-non.svg';
-        img.src = `/images/svg/favorites/${imgPath}`;
-      });
-  }
+        const email = <HTMLInputElement> document.getElementById('email');
+        const id = elt.id.substr(9,elt.id.length);
 
+        fetch(`/api/favorites/${email.value}/talks/${id}/toggle`, {method: 'post'})
+            .then(response => response.json())
+            .then((json: any) => {
+                const imgPath = json.selected ? 'mxt-favorite.svg' : 'mxt-favorite-non.svg';
+                if(targetIsButton){
+                    elt.innerHTML = `<img src="/images/svg/favorites/${imgPath}" class="mxt-icon--cat__talks" id="favorite-{{id}}"/>`;
+                }
+                else {
+                    elt.src = `/images/svg/favorites/${imgPath}`;
+                }
+            });
+
+        event.stopPropagation();
+    }
 }
 
 window.addEventListener("load", () => window['ctrl'] = new TalksCtrl());
