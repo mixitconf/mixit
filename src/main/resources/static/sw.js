e1d7edb34f0f064790d448db4e0726009dc7643d
Guillaume EHRET <guillaume@dev-mind.fr>
2018-10-18 20:06:25 +0200

    Update service worker configuration

--- src/main/resources/static/sw.js
+++ src/main/resources/static/sw.js
@@ -8,79 +8,77 @@ if (workbox) {
 
     workbox.precaching.precacheAndRoute([]);
 
-    workbox.routing.registerRoute(
-        new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
-        workbox.strategies.cacheFirst({
-            cacheName: 'googleapis',
-            networkTimeoutSeconds: 3,
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 30
-                })
-            ]
-        })
-    );
+  workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst(
+      {
+        cacheName: 'googleapis',
+        networkTimeoutSeconds: 3,
+        plugins: [
+          new workbox.cacheableResponse.Plugin({ statuses: [0, 200] }),
+          new workbox.expiration.Plugin({ maxEntries: 30, purgeOnQuotaError: true})
+        ]
+      })
+  );
 
-    workbox.routing.registerRoute(
-        /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
-        workbox.strategies.cacheFirst({
-            cacheName: 'images',
-            networkTimeoutSeconds: 3,
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 60,
-                    maxAgeSeconds: 6 * 60 * 60
-                })
-            ]
-        })
-    );
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+    workbox.strategies.cacheFirst(
+      {
+        cacheName: 'images',
+        networkTimeoutSeconds: 3,
+        plugins: [
+          new workbox.expiration.Plugin({ maxEntries: 60, maxAgeSeconds: 6 * 60 * 60, purgeOnQuotaError: true})
+        ]
+      })
+  );
 
-    // use a stale while revalidate for CSS and JavaScript files that aren't precached.
-    workbox.routing.registerRoute(/\.(?:js|css)$/, workbox.strategies.staleWhileRevalidate({
-            cacheName: 'static-resources',
-            networkTimeoutSeconds: 3,
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 60,
-                    maxAgeSeconds: 6 * 60 * 60
-                })
-            ]
-        })
-    );
+  // use a stale while revalidate for CSS and JavaScript files that aren't precached.
+  workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    workbox.strategies.staleWhileRevalidate(
+      {
+        cacheName: 'static-resources',
+        networkTimeoutSeconds: 3,
+        plugins: [
+          new workbox.expiration.Plugin(
+            {
+              maxEntries: 60,
+              maxAgeSeconds: 60 * 60
+            })
+        ]
+      })
+  );
 
-    workbox.routing.registerRoute(/(.*)\/admin(.*)$/, workbox.strategies.staleWhileRevalidate({
-            cacheName: 'html-adm-resources',
-            networkTimeoutSeconds: 3,
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 1,
-                    maxAgeSeconds: 1
-                }),
-                new workbox.cacheableResponse.Plugin({
-                    headers: {
-                        'Content-Type': 'text/html;charset=UTF-8'
-                    }
-                })
-            ]
-        })
-    );
+  // use a stale while revalidate for CSS and JavaScript files that aren't precached.
+  workbox.routing.registerRoute(
+    /\.(?:html)$/,
+    workbox.strategies.networkFirst(
+      {
+        cacheName: 'html-resources',
+        networkTimeoutSeconds: 4,
+        plugins: [
+          new workbox.expiration.Plugin({ maxEntries: 60, maxAgeSeconds: 30 * 60 })
+        ]
+      })
+  );
 
-    workbox.routing.registerRoute(/.*$/, workbox.strategies.staleWhileRevalidate({
-            cacheName: 'html-resources',
-            networkTimeoutSeconds: 3,
-            plugins: [
-                new workbox.expiration.Plugin({
-                    maxEntries: 80,
-                    maxAgeSeconds: 6 * 60 * 60
-                }),
-                new workbox.cacheableResponse.Plugin({
-                    headers: {
-                        'Content-Type': 'text/html;charset=UTF-8'
-                    }
-                })
-            ]
-        })
-    );
+  workbox.routing.registerRoute(/(.*)\/admin(.*)$/, workbox.strategies.staleWhileRevalidate({
+          cacheName: 'html-adm-resources',
+          networkTimeoutSeconds: 3,
+          plugins: [
+              new workbox.expiration.Plugin({
+                  maxEntries: 1,
+                  maxAgeSeconds: 1
+              }),
+              new workbox.cacheableResponse.Plugin({
+                  headers: {
+                      'Content-Type': 'text/html;charset=UTF-8'
+                  }
+              })
+          ]
+      })
+  );
 }
 else{
     console.error('Error on workbox initialization');
