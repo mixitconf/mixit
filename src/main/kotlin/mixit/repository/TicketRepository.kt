a5f90636e2f9bb0d0653750d5453e0bfa3d66ecd
Guillaume EHRET <guillaume@dev-mind.fr>
2019-03-14 11:59:15 +0100

    Ticket reset is deleted

--- src/main/kotlin/mixit/repository/TicketRepository.kt
+++ src/main/kotlin/mixit/repository/TicketRepository.kt
@@ -26,7 +26,6 @@ class TicketRepository(private val template: ReactiveMongoTemplate,
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     fun initData() {
-        deleteAll().block()
         if (count().block() == 0L) {
             val usersResource = ClassPathResource("data/ticket.json")
             val tickets: List<Ticket> = objectMapper.readValue(usersResource.inputStream)
