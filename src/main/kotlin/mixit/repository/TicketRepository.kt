4ac7b0ffb2ac94b2258c9ba7b4cc7222e162b14d
Charles Bouttaz <c.bouttaz@gmail.com>
2020-02-12 17:18:21 +0100

    Revert "Prepare lottery: remove all tickets from 2019"
    This reverts commit 74c374ff

--- src/main/kotlin/mixit/repository/TicketRepository.kt
+++ src/main/kotlin/mixit/repository/TicketRepository.kt
@@ -19,7 +19,6 @@ class TicketRepository(private val template: ReactiveMongoTemplate,
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     fun initData() {
-        deleteAll().block()
         if (count().block() == 0L) {
             val usersResource = ClassPathResource("data/ticket.json")
             val tickets: List<Ticket> = objectMapper.readValue(usersResource.inputStream)
