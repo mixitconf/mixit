c281494f64d59448d88d0311359b0bf68e474f7d
Guillaume EHRET <guillaume@dev-mind.fr>
2021-11-19 21:36:35 +0100

    Fix style error

--- src/main/kotlin/mixit/config/DatabaseInitializerConfig.kt
+++ src/main/kotlin/mixit/config/DatabaseInitializerConfig.kt
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class DatabaseConfig {
+class DatabaseInitializerConfig {
 
     @Bean
     fun databaseInitializer(
