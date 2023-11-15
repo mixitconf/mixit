6439e18f46df7fa94df1d024dab9453be7b7df70
Guillaume EHRET <guillaume@dev-mind.fr>
2023-11-15 23:27:06 +0100

    Migrate to gradle 8.4, Java 21 and Spring 3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,19 @@ import com.github.gradle.node.npm.task.NpmTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    val kotlinVersion = "1.6.21"
+    val kotlinVersion = "1.9.20"
 
-    id("org.springframework.boot") version "2.7.10"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.3"
     id("com.github.node-gradle.node") version "3.1.1"
     id("org.jmailen.kotlinter") version "3.7.0"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
-    kotlin("kapt") version kotlinVersion
+   // kotlin("kapt") version kotlinVersion
 }
 
-version = "2023.0.0-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+version = "2024.0.0-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ dependencies {
     val commonmarkVersion = "0.11.0"
     val gmailApiVersion = "1.24.2"
     val googleApiVersion = "v1-rev101-1.24.1"
-    val googleOwaspVersion ="20171016.1"
+    val googleOwaspVersion = "20211018.1"
 
     implementation("org.springframework.boot:spring-boot-starter-webflux") {
         exclude(module = "hibernate-validator")
@@ -51,7 +51,7 @@ dependencies {
     implementation("com.google.apis:google-api-services-gmail:$googleApiVersion")
     implementation("com.googlecode.owasp-java-html-sanitizer:owasp-java-html-sanitizer:$googleOwaspVersion")
 
-    kapt("org.springframework.boot:spring-boot-configuration-processor")
+    //kapt("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
@@ -64,7 +64,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
 }
 
@@ -87,9 +87,9 @@ tasks.register<Copy>("copyImages") {
 tasks.register<NpmTask>("compileSass") {
     dependsOn(tasks.npmInstall)
     npmCommand.set(listOf("run", "scss"))
-    args.set(listOf("--", "--out-dir", "${buildDir}/npm-output"))
+    args.set(listOf("--", "--out-dir", "${layout.buildDirectory}/npm-output"))
     inputs.dir("src/main/sass")
-    outputs.dir("${buildDir}/resources/main/static/css")
+    outputs.dir("${layout.buildDirectory}/resources/main/static/css")
 }
 
 tasks.register<Copy>("copyJsVendors") {
@@ -104,9 +104,9 @@ tasks.register<Copy>("copyJsVendors") {
 tasks.register<NpmTask>("compileTypescript") {
     dependsOn(tasks.npmInstall)
     npmCommand.set(listOf("run", "typescript"))
-    args.set(listOf("--", "--out-dir", "${buildDir}/npm-output"))
+    args.set(listOf("--", "--out-dir", "${layout.buildDirectory}/npm-output"))
     inputs.dir("src/main/ts")
-    outputs.dir("${buildDir}/resources/main/static/js")
+    outputs.dir("${layout.buildDirectory}/resources/main/static/js")
 }
 
 /**
@@ -118,7 +118,7 @@ tasks.register<NpmTask>("compileTypescript") {
 tasks.register<NpmTask>("watch") {
     dependsOn(tasks.npmInstall)
     npmCommand.set(listOf("run", "watch"))
-    args.set(listOf("--", "--out-dir", "${buildDir}/npm-output"))
+    args.set(listOf("--", "--out-dir", "${layout.buildDirectory}/npm-output"))
 }
 
 tasks.getByName("processResources").dependsOn(
