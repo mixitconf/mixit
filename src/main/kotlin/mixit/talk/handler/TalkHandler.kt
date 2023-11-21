cde39fb0d3ad463457cd60b489a88b65cd05dc3d
Guillaume EHRET <guillaume@dev-mind.fr>
2023-11-21 09:37:19 +0100

    New MiXiT design system

--- src/main/kotlin/mixit/talk/handler/TalkHandler.kt
+++ src/main/kotlin/mixit/talk/handler/TalkHandler.kt
@@ -15,6 +15,7 @@ import mixit.routes.MustacheI18n.SPONSORS
 import mixit.routes.MustacheI18n.TALKS
 import mixit.routes.MustacheI18n.TITLE
 import mixit.routes.MustacheI18n.YEAR
+import mixit.routes.MustacheI18n.YEAR_SELECTOR
 import mixit.routes.MustacheTemplate
 import mixit.routes.MustacheTemplate.FeedbackWall
 import mixit.routes.MustacheTemplate.Media
@@ -33,6 +34,7 @@ import mixit.talk.model.TalkFormat
 import mixit.talk.model.TalkService
 import mixit.user.handler.dto.toDto
 import mixit.user.model.UserService
+import mixit.util.YearSelector
 import mixit.util.currentNonEncryptedUserEmail
 import mixit.util.enumMatcher
 import mixit.util.errors.NotFoundException
@@ -124,20 +126,26 @@ class TalkHandler(
 
     private data class TalkKey(val date: String, val id: String = date.replace(" ", "").lowercase())
 
+
     suspend fun findByEventView(config: TalkViewConfig): ServerResponse {
         val currentUserEmail = config.req.currentNonEncryptedUserEmail()
         val talks = filterTalkByFormat(
             loadTalkAndFavorites(config, currentUserEmail).let { talks ->
-                if (config.template == Media) {
-                    talks.filter { it.video != null }
-                } else if (config.template == FeedbackWall) {
-                    talks
-                        .filterNot { it.format == TalkFormat.INTERVIEW }
-                        .filterNot { it.format == TalkFormat.ON_AIR }
-                        .filterNot { it.topic == "onair" }
-                        .filterNot { it.title.contains("Keynote team") }
-                        .toList()
-                } else talks
+                when (config.template) {
+                    Media -> {
+                        talks.filter { it.video != null }
+                    }
+                    FeedbackWall -> {
+                        talks
+                            .asSequence()
+                            .filterNot { it.format == TalkFormat.INTERVIEW }
+                            .filterNot { it.format == TalkFormat.ON_AIR }
+                            .filterNot { it.topic == "onair" }
+                            .filterNot { it.title.contains("Keynote team") }
+                            .toList()
+                    }
+                    else -> talks
+                }
             },
             config.viewWorkshop
         )
@@ -159,6 +167,7 @@ class TalkHandler(
                 mapOf(
                     EVENT to event.toEvent(),
                     SPONSORS to userService.loadSponsors(event),
+                    YEAR_SELECTOR to YearSelector.create(config.year, config.template.template, talk = true),
                     TALKS to when (config.viewList) {
                         SimpleList -> talks
                         Agenda -> {
